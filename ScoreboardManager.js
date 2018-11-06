f0ce13b5b1bf5936a1a03afafcdb1466b1ac3128
AaronCur <c00202505@itcarlow.ie>
2018-11-06 11:00:22 +0000

    Added tests

--- ScoreboardManager.js
+++ ScoreboardManager.js
@@ -3,55 +3,44 @@ class ScoreboardManager
   constructor()
   {
     this.scoreboard =  []
-
-
     this.count = 0;
-    this.score = 0;
     this.playerName = "";
 
   }
 
-  initBoard(object){
+  initBoard(object)
+  {
 
     this.scoreboard = object
     console.log(this.scoreboard)
   }
-
+  //Takes user input, adds the score which is passed
+  //to the function along with a userID which is created
+  //To the leaderboardd as an object in the form {name: "Aaron", score:100, playerID:1}
   addToBoard(score)
   {
 
     if(this.count <1)
     {
-      while (this.playerName == "") {
+      while (this.playerName == "")
+      {
           this.playerName = prompt ("Please enter your name","");
       }
-    this.playerID = this.scoreboard.length + 1;
-    var object = {name: this.playerName, score: score, playerID: this.playerID}
-    this.scoreboard.push(object);
-
-
-
-      console.log(this.scoreboard)
+      this.playerID = this.scoreboard.length + 1;
+      var object = {name: this.playerName, score: score, playerID: this.playerID}
+      this.scoreboard.push(object);
       this.count = this.count + 1;
     }
-
 
   }
 
   getBoard()
   {
-
+    return this.scoreboard
   }
 
-  displayBoard()
+  filterName(name)
   {
-
-  }
-
-  startTimer(){
-
-  }
-  filterName(name){
     //Filter
     var wanted = this.scoreboard.filter(function(scoreboard)
     {
