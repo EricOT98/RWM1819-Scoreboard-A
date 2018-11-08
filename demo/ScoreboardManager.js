27289a9e8730a31ca6e8b3649d29fe63d8e7d6ca
AaronCur <c00202505@itcarlow.ie>
2018-11-08 11:12:10 +0000

    Completed feature 2

--- demo/ScoreboardManager.js
+++ demo/ScoreboardManager.js
@@ -3,16 +3,60 @@ class ScoreboardManager
   constructor()
   {
     this.scoreboard =  []
-    this.count = 0;
-    this.playerName = "";
+    this.count = 0
+    this.playerName = ""
+    this.session = false
+    this.local = false
+
+    this.storageType = "none";
 
   }
-
-  initBoard(object)
+  clearLocalStorage()
   {
 
-    this.scoreboard = object
-    console.log(this.scoreboard)
+    localStorage.removeItem('Scoreboard');
+
+  }
+  clearSessionStorage()
+  {
+    sessionStorage.removeItem('Scoreboard');
+  }
+  initBoard(storageType)
+  {
+
+    if(storageType == "local" || storageType == "Local"){
+      this.storageType = "local"
+      if((localStorage.getItem('Scoreboard') === null))
+      {
+
+        localStorage.setItem('Scoreboard', JSON.stringify(this.scoreboard));
+      }
+      this.scoreboard = JSON.parse(localStorage.getItem('Scoreboard'));
+
+      if(this.scoreboard == null)
+      {
+        this.scoreboard = []
+      }
+    }
+    else if (storageType == "session" || storageType == "Session") {
+      this.storageType = "session"
+      if((sessionStorage.getItem('Scoreboard') === null))
+      {
+
+        sessionStorage.setItem('Scoreboard', JSON.stringify(this.scoreboard));
+      }
+      this.scoreboard = JSON.parse(sessionStorage.getItem('Scoreboard'));
+
+      if(this.scoreboard == null)
+      {
+        this.scoreboard = []
+      }
+    }
+    else{
+      console.log("Please enter a valid storage type")
+    }
+
+
   }
   //Takes user input, adds the score which is passed
   //to the function along with a userID which is created
@@ -22,14 +66,24 @@ class ScoreboardManager
 
     if(this.count <1)
     {
-      while (this.playerName == "")
+      while (this.playerName === "")
       {
           this.playerName = prompt ("Please enter your name","");
       }
-      this.playerID = this.scoreboard.length + 1;
-      var object = {name: this.playerName, score: score, playerID: this.playerID}
-      this.scoreboard.push(object);
-      this.count = this.count + 1;
+
+        this.playerID = this.scoreboard.length + 1
+        var object = {name: this.playerName, score: score, playerID: this.playerID}
+        this.scoreboard.push(object);
+
+        switch(this.storageType) {
+            case "local":
+            localStorage.setItem('Scoreboard', JSON.stringify(this.scoreboard));
+              break;
+            case "session":
+            sessionStorage.setItem('Scoreboard', JSON.stringify(this.scoreboard));
+              break;
+      }
+        this.count = this.count + 1;
     }
 
   }
--- demo/game.js
+++ demo/game.js
@@ -4,7 +4,8 @@ class Game
   {
 
     this.scoreboard = new ScoreboardManager();
-
+    this.scoreboard.clearLocalStorage();
+    this.scoreboard.initBoard("Local");
     this.score = 100;
     this.scoreboard.addToBoard(this.score);
     console.log(this.scoreboard.getBoard());
